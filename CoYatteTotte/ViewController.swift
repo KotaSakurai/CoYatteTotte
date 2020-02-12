7334e579f928dfb5f1e91e63e8d707188005027a
KotaSakurai <sakurai.kota@mwed.co.jp>
2020-02-13 02:04:31 +0900

    Initial Commit

---
+++ CoYatteTotte/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoYatteTotte
+//
+//  Created by 桜井広大 on 2020/02/13.
+//  Copyright © 2020 KotaSakurai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
